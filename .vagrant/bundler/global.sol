3838ed56b3889e04d9a11fd4684b8e71e12f0eac
ddiaz <ddiaz@localhost.localdomain>
2021-04-08 12:48:20 -0600

    Testing jenkins and vagrant

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-omnibus",["= 1.5.0"]],["mini_portile2",["~> 2.4.0"]],["nokogiri",["~> 1.6"]],["iniparse",["> 1.0"]],["log4r",["~> 1.1"]],["concurrent-ruby",["~> 1.0"]],["i18n",["~> 1.0"]],["vagrant-vmware-esxi",["= 2.5.2"]]],"checksum":"0f6e2d19c0de7af1a7443533c10aa1165573c08c7501ca69751c303dbb3f4239","vagrant_version":"2.2.14"}
