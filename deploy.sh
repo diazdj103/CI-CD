80bb323f3fc02bf1f6610e713fa39981dad084e4
Daniel <danieldiaz648@gmail.com>
2021-04-10 11:15:59 -0600

    Trying environment variables for good template vagrant file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
  #!/bin/bash
-branch= $(env -i git rev-parse --abbrev-ref HEAD)
+branch="environment"
+
+function getSomeString {
+ git rev-parse --abbrev-ref HEAD
+}
+getSomeString branch
 echo "This is the $branch branch" 
 vagrant  up $branch
