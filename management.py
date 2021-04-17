910a9165283c71f5695ea601fbdc15794dd05467
Daniel's laptop <danieldiaz648@gmail.com>
2021-04-16 21:13:37 -0600

    updating for environments

--- management.py
+++ management.py
@@ -12,15 +12,15 @@ def determine_environment (environment):
     if environment == "development":
         logging.info("Depploying the development environment")
         os.system("vagrant up development-vm1")
-        os.system ("sleep 30")
         os.system("vagrant up development-vm2")
 
-    if environment != "development" or "main" or "production":
-        logging.error("Invalid choice")
+    elif environment == "main":
+        os.system("vagrant up main-vm1")
+        os.system("vagrant up main-vm2")
 
-    elif environment == "development":
-        logging.info("Depploying the development environment")
-        
+     elif environment == "production":
+        os.system("vagrant up production-vm1")
+        os.system("vagrant up production-vm2")
 
     else:
         ""
