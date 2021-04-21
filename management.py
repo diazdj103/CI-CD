d25965e54575ee833105975fbea5c1d70c1eaee8
Daniel Diaz <danieldiaz648@gmail.com>
2021-04-20 19:21:15 -0600

    updating file

--- management.py
+++ management.py
@@ -11,20 +11,24 @@ def determine_environment (environment):
         logging.info("Depploying the development environment")
         if os.system("vagrant up development-vm1") !=0:
             logging.exception("Vagrant failed to deploy")
-        else:
-            return True
         if os.system("vagrant up development-vm2") !=0:
             logging.exception("Vagrant failed to deploy")
+        else:
+            return True
     elif environment == "main":
         if  os.system("vagrant up main-vm1") !=0:
            logging.exception("Vagrant failed to deploy")
         if os.system("vagrant up main-vm2") !=0:
            logging.exception("Vagrant failed to deploy")
+        else:
+            return True
     elif environment == "production":
         if os.system("vagrant up production-vm1") !=0:
             logging.exception("Vagrant failed to deploy")
         if os.system("vagrant up production-vm2") !=0:
             logging.exception("Vagrant failed to deploy")
+        else:
+            return True
     else:
         logging.exception("The wrong environment was called")
 
