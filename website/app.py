678e787589dbb4e07f5d869eb5a5a4ee697db851
Daniel Diaz <danieldiaz648@gmail.com>
2021-06-03 13:27:24 -0600

    Building application

---
+++ website/app.py
@@ -0,0 +1,13 @@
+from flask import Flask , render_template
+
+#  __name__ is a convenient way to get the import name of the place the app is defined.
+# Flask uses the import name to know where to look up resources, templates, static files, instance folder, etc
+app = Flask(__name__)
+
+@app.route("/index")
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+
+    app.run(host='localhost',port='5000')
